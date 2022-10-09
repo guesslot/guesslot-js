c3e6ee9744bb145bb36ee4d5252a97df0ebe1d66
Guesslot <guesslot@outlook.com>
2022-10-09 17:49:49 +0800

    update

--- src/constants.ts
+++ src/constants.ts
@@ -1,23 +1,23 @@
 export const Settings: any = {
   MetaMask: {
     Chain: {
-      chainId: '0x4',
+      chainId: '0x5',
     },
   },
   NoWallet: {
-    chainId: '0x4',
-    rpc: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    chainId: '0x5',
+    rpc: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   },
   WalletConnect: {},
 
   Contracts: {
-    4: {
-      Stats: '0x1f7bF828E710408180bC9447fcd0729D41Fd53CE',
+    5: {
+      Stats: '0x11F2B4470d40051363C7e03aCb5d04d4EFf88a72',
       Positions: {
         'gUSDT-BTC': {
           token: 'gUSDT',
           asset: 'BTC',
-          pool: '0x77A51af62e8eF87D5245c4994Cc6C0e028AaA454',
+          pool: '0xB8dab01E3a407966BF1b78342A7689B45a347198',
         },
       },
     },
